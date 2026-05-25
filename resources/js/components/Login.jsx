e7e153410c73ef862d4ee4dc73178c43a2ef3d61
Kevin Tapias <kevinjvillalba774+1@gmail.com>
2026-05-24 21:08:41 -0500

    deploy config

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
 
         setError('')
 
-        const response = await fetch('http://localhost:8000/api/login', {
+        const response = await fetch('https://mificho.onrender.com/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password: passw })
